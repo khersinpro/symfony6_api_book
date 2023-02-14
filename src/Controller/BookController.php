719e23be40617fb688c5d766433c9ce010f650bb
khersinpro <khersinpro@gmail.com>
2023-02-14 15:02:26 +0100

    add nelmio documentation

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -18,8 +18,18 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
+
 class BookController extends AbstractController
 {
+    /**
+     * Cette méthode permet de récupérer l'ensemble des livres. params => ?page= &limit=
+     * 
+     * @param BookRepository $bookRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @param TagAwareCacheInterface $cache
+     * @return JsonResponse
+     */
     #[Route('/api/book', name: 'book.get.all', methods: ['GET'])]
     public function getBookList(BookRepository $bookRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cache,
         VersioningService $versioningService
@@ -41,6 +51,13 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
 
+
+    /**
+     * Cette méthode permet de récuperer un livre depuis son ID
+     * @param Book $book
+     * @param SerializerInterface $serializer
+     * @param VersioningService $versioningService
+     */
     #[Route('/api/book/{id}', name: 'book.get.one', methods: ['GET'])]
     public function getDetailBook(Book $book, SerializerInterface $serializer, VersioningService $versioningService): JsonResponse
     {
@@ -52,6 +69,15 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
+    /**
+     * Cette méthode permet de créer un livre 
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @param BookRepository $bookRepository
+     * @param AuthorRepository $authorRepository
+     * @param ValidatorInterface $validator
+     * @param TagAwareCacheInterface $cache
+     */
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/api/book/create', name: 'book.create.one', methods: ['POST'])]
     public function createBook(SerializerInterface $serializer, Request $request, BookRepository $bookRepository, AuthorRepository $authorRepository,
@@ -73,6 +99,16 @@ class BookController extends AbstractController
         return new JsonResponse(null, Response::HTTP_CREATED, []);
     }
     
+    /**
+     * Cette méthode permet de modifier un livre
+     * @param Book $currentBook,
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @param BookRepository $bookRepository
+     * @param AuthorRepository $authorRepository
+     * @param ValidatorInterface $validator
+     * @param TagAwareCacheInterface $cache
+     */
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/api/book/update/{id}', name: 'book.update.one', methods: ['PUT'])]
     public function updateBook(Book $currentBook, SerializerInterface $serializer, Request $request, ValidatorInterface $validator, 
@@ -98,9 +134,16 @@ class BookController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NO_CONTENT, ['accept' => 'json']);
     }
 
+
+    /**
+     * Cette méthode permet de supprime un livre
+     * @param Book $book
+     * @param BookRepository $bookRepository
+     * @param TagAwareCacheInterface $cache
+     */
     #[Route('/api/book/delete/{id}', name: 'book.delete.one', methods: ['DELETE'])]
     #[IsGranted("ROLE_ADMIN")]
-    public function deleteBook(Book $book, SerializerInterface $serializer, BookRepository $bookRepository, TagAwareCacheInterface $cache): JsonResponse
+    public function deleteBook(Book $book, BookRepository $bookRepository, TagAwareCacheInterface $cache): JsonResponse
     {
         $bookRepository->remove($book, true);
         $cache->invalidateTags(['booksCache']);
